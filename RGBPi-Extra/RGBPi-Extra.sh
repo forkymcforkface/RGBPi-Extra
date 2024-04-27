3e7e34ec80e6e76ccd812f1a1d60b6eaab51f36b
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-04-26 23:43:11 -0700

    Update RGBPi-Extra.sh

--- RGBPi-Extra/RGBPi-Extra.sh
+++ RGBPi-Extra/RGBPi-Extra.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 cd "$(dirname "$(readlink -f "$0")")"
-python application/main.py
+if grep -q bookworm /etc/os-release; then
+    python3.9 application/main.py
+else
+    python3 application/main.py
+fi
