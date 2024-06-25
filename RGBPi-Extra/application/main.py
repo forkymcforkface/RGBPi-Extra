2bd598398cb28dc73761e6aa234989cb05ec08aa
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-06-25 12:54:51 -0700

    Update main.py

--- RGBPi-Extra/application/main.py
+++ RGBPi-Extra/application/main.py
@@ -1,6 +1,9 @@
+import shutil
 import os
 import pygame
 import pygame_menu
+import time
+from retroarch_settings import get_retroarch_settings_menu
 from rgbpi_tweaks import get_rgbpi_tweaks_menu
 from core_updater import get_core_updater_menu
 from core_swapper import get_core_swap_menu
@@ -9,6 +12,8 @@ from tweaks_settings import get_tweaks_settings_menu
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 RGBPI_UI_ROOT = '/opt/rgbpi/ui'
+PATCH_FLAG_FILE = os.path.join(RGBPI_UI_ROOT, 'patch_applied.flag')
+VERSION = 'v.20a'
 
 WINDOW_SIZE = (290, 240)
 
@@ -27,10 +32,9 @@ menu_theme.widget_alignment = pygame_menu.locals.ALIGN_LEFT
 menu_theme.scrollbar_color = (0, 0, 0, 255)
 menu_theme.scrollbar_slider_color = (0, 0, 0, 255)
 menu_theme.widget_padding = (1, 2)
-menu_theme.title_alignment = pygame_menu.locals.ALIGN_RIGHT  # Align title to the right
 
 menu = pygame_menu.Menu(
-    title='RGBPi Extra v.20a',
+    title=f'RGBPi Extra {VERSION}',
     theme=menu_theme,
     joystick_enabled=True,
     width=WINDOW_SIZE[0],
@@ -38,16 +42,65 @@ menu = pygame_menu.Menu(
     mouse_visible_update=False,
 )
 
+def apply_patch():
+    error = None
+    try:
+        shutil.copytree('data/shaders', '/root/.config/retroarch/shaders', dirs_exist_ok=True)
+        shutil.copytree('data/cores', '/opt/retroarch/cores', dirs_exist_ok=True)
+
+        with open('data/cores.cfg', 'r') as source_file:
+            data_to_append = source_file.read()
+        with open('/opt/rgbpi/ui/data/cores.cfg', 'r+') as dest_file:
+            lines = dest_file.readlines()
+            dest_file.seek(0)
+            for line in lines:
+                if not line.startswith(('melonds_', 'yabasanshiro_')):
+                    dest_file.write(line)
+            dest_file.write(data_to_append)
+            dest_file.truncate()
+
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        drive = script_dir.split(os.sep)[2]
+        media_mountpoint = os.path.join('/', 'media', drive)
+
+        source_dir = os.path.join(os.path.dirname(__file__), 'data', 'drive')
+        shutil.copytree(source_dir, media_mountpoint, dirs_exist_ok=True)
+
+        with open(PATCH_FLAG_FILE, 'w') as f:
+            f.write(VERSION)
+    except Exception as e:
+        error = 'patch'
+    load_menu(error=error)
+
 def load_menu(error=None):
     menu.clear()
-    rgbpi_tweaks_menu = get_rgbpi_tweaks_menu(menu_theme, WINDOW_SIZE)
-    settings_menu = get_tweaks_settings_menu(menu_theme, WINDOW_SIZE)                
-    menu.add.button('Tweaks', rgbpi_tweaks_menu)
-    menu.add.button('Core Swapper (do not use)', get_core_swap_menu(menu_theme, WINDOW_SIZE))    
-    menu.add.button('Core Updater', get_core_updater_menu(menu_theme, WINDOW_SIZE))
-    menu.add.button('Settings', settings_menu)
-    menu.add.vertical_margin(margin=10)
-    menu.add.button('Quit', pygame_menu.events.EXIT)
+    if error:
+        if error == 'patch':
+            menu.add.label('Error applying patch!', wordwrap=False)
+        menu.add.vertical_margin(margin=10)
+        menu.add.button('OK', load_menu)
+    else:
+        patch_needed = True
+        if os.path.exists(PATCH_FLAG_FILE):
+            with open(PATCH_FLAG_FILE, 'r') as f:
+                applied_version = f.read().strip()
+                if applied_version >= VERSION:
+                    patch_needed = False
+
+        if patch_needed:
+            menu.add.button('Install RGBPi-Extra', apply_patch)
+        else:
+            retroarch_settings_menu = get_retroarch_settings_menu(menu_theme, WINDOW_SIZE)
+            rgbpi_tweaks_menu = get_rgbpi_tweaks_menu(menu_theme, WINDOW_SIZE)
+            settings_menu = get_tweaks_settings_menu(menu_theme, WINDOW_SIZE)                
+            menu.add.button('Retroarch Settings', retroarch_settings_menu)
+            menu.add.button('Core Updater', get_core_updater_menu(menu_theme, WINDOW_SIZE))
+            menu.add.button('Core Swapper', get_core_swap_menu(menu_theme, WINDOW_SIZE))
+            menu.add.button('Tweaks', rgbpi_tweaks_menu)
+            menu.add.button('Settings', settings_menu)
+        menu.add.vertical_margin(margin=10)
+        menu.add.button('Quit', pygame_menu.events.EXIT)
+
     pygame.display.update()
 
 load_menu()
@@ -61,6 +114,5 @@ if __name__ == '__main__':
         if menu.is_enabled():
             menu.update(events)
             menu.draw(surface)
-        else:
-            break
+        time.sleep(0.01) 
         pygame.display.update()
