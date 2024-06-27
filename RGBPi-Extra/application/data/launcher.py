35f43803e0c0005f63c0dedd41386d659a3cf9cb
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-06-26 17:11:12 -0700

    Update launcher.py

--- RGBPi-Extra/application/data/launcher.py
+++ RGBPi-Extra/application/data/launcher.py
@@ -21,24 +21,34 @@ def launch_content():
     systems_cores = read_systems_cores('systems_cores.cfg')
     system = cglobals.launcher['system']
     if system not in systems_cores:
-        return launch_content2()   
+        return launch_content2()
+    
     game_path = cglobals.launcher['game_path']
     return_view = cglobals.launcher['return_view']
-    is_global_nfs = utils.is_global_nfs()   
+    is_global_nfs = utils.is_global_nfs()
+    
     try:
         cglobals.sound_mgr.pause_music()
-        pygame.display.quit()        
+        pygame.display.quit()
+        
         if system in cglobals.presets:
-            cglobals.sound_mgr.set_preset(preset=system)       
+            cglobals.sound_mgr.set_preset(preset=system)
+        
         retroarch_cfg_file = make_console_cfg_file(system, is_global_nfs)
-        device_cmd = set_device_type(system, game_path)        
-        utils.cmd('clear')        
+        device_cmd = set_device_type(system, game_path)
+        
+        utils.cmd('clear')
+        
         core = systems_cores[system]
-        launch_command = f'{rtk.path_retroarch} {device_cmd} -L {rtk.path_cores}/{core} --appendconfig={retroarch_cfg_file} "{game_path}"'
+        if system == 'nds':
+            launch_command = f'{rtk.path_cores}/launchnds.bash --appendconfig={retroarch_cfg_file} "{game_path}"'
+        else:
+            launch_command = f'{rtk.path_retroarch} {device_cmd} -L {rtk.path_cores}/{core} --appendconfig={retroarch_cfg_file} "{game_path}"'
+        
         utils.cmd(launch_command)
         utils.write_stats(start=time.time(), end=time.time())
     except Exception as error:
-        pass
+        rtk.logging.error('Error in [Launcher]: %s', error)
     finally:
         rtk.init_video()
         utils.reset_key_pressed()
