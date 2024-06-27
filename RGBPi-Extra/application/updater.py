c04ee0c901add3b5a52a35aeb6431197ec95c4de
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-06-27 10:13:59 -0700

    Update updater.py

--- RGBPi-Extra/application/updater.py
+++ RGBPi-Extra/application/updater.py
@@ -1,14 +1,12 @@
 import os
-import tarfile
 import shutil
 import subprocess
-import urllib.request
 import pygame
-import glob
+import tempfile
 
-def display_loading_screen(screen, font, message):
+def display_loading_screen(screen, font):
     screen.fill(BLACK)
-    text_surface = font.render(message, True, WHITE)
+    text_surface = font.render('Downloading RGBPi-Extra', True, WHITE)
     screen.blit(text_surface, (WINDOW_SIZE[0] // 2 - text_surface.get_width() // 2,
                                WINDOW_SIZE[1] // 2 - text_surface.get_height() // 2))
     pygame.display.flip()
@@ -23,57 +21,56 @@ font = pygame.font.Font(pygame.font.get_default_font(), 14)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+display_loading_screen(screen, font)
+
 repo_owner = "forkymcforkface"
 repo_name = "RGBPi-Extra"
-branch = "dev"
+branch = "main"
 path = "RGBPi-Extra"
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.abspath(os.path.join(script_dir, os.pardir))
-grandparent_dir = os.path.abspath(os.path.join(parent_dir, os.pardir))
-os.chdir(grandparent_dir)
-
-archive_file = os.path.join(grandparent_dir, f"{repo_name}-{branch}.tar.gz")
-if os.path.exists(archive_file):
-    os.remove(archive_file)
-
-archive_url = f"https://github.com/{repo_owner}/{repo_name}/archive/{branch}.tar.gz"
-display_loading_screen(screen, font, "Downloading Update")
+python_exec = 'python'
 try:
-    urllib.request.urlretrieve(archive_url, archive_file)
-except KeyboardInterrupt:
-    display_loading_screen(screen, font, "Download Interrupted, Rebooting")
-    pygame.time.wait(5000) 
-    os.system('reboot')
-except urllib.error.URLError:
-    display_loading_screen(screen, font, "Download Failed, Rebooting")
-    pygame.time.wait(5000)  
-    os.system('reboot')
-except Exception as e:
-    display_loading_screen(screen, font, "Check Internet Connection, Rebooting")
-    pygame.time.wait(5000)  
-    os.system('reboot') 
+    with open('/etc/os-release') as f:
+        os_release = f.read()
+    if 'bookworm' in os_release:
+        python_exec = 'python3.9'
+        branch = "pi-5"
+except Exception:
+    pass
 
-temp_dir = os.path.join(grandparent_dir, "rgbpitemp")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+temp_dir = os.path.join(script_dir, "rgbpitemp")
 os.makedirs(temp_dir, exist_ok=True)
-with tarfile.open(archive_file, "r:gz") as tar:
-    tar.extractall(path=temp_dir)
-
-source_dir = os.path.join(temp_dir, f"{repo_name}-{branch}", path)
-destination_dir = os.path.join(grandparent_dir, path)
-
-if os.path.exists(destination_dir):
-    shutil.rmtree(destination_dir)
-
+clone_cmd = ['git', 'clone', f'https://github.com/{repo_owner}/{repo_name}', '-b', branch, '--depth', '1', temp_dir]
+subprocess.run(clone_cmd, check=True)
+source_dir = os.path.join(temp_dir, path)
+destination_dir = os.path.join(script_dir, path)
 shutil.move(source_dir, destination_dir)
-
 shutil.rmtree(temp_dir)
-os.remove(archive_file)
 
-with subprocess.Popen(['df', '-P', grandparent_dir], stdout=subprocess.PIPE) as proc:
+with subprocess.Popen(['df', '-P', script_dir], stdout=subprocess.PIPE) as proc:
     output = proc.stdout.readlines()
     mount_point = output[1].decode().split()[5]
 
-display_loading_screen(screen, font, "Update Complete, Rebooting")
-pygame.time.wait(5000)  
-os.system('reboot')
+games_dat = os.path.join(mount_point, 'dats', 'games.dat')
+
+data_to_insert = '"","","ports","ports","/roms/ports/RGBPi-Extra/RGBPi-Extra.sh","RGBPi-Extra","?","?","19XX","1"\n'
+
+with open(games_dat, 'r+') as f:
+    lines = f.readlines()
+    f.seek(0)
+    for line in lines:
+        if 'Install RGBPi-Extra.sh' not in line:
+            f.write(line)
+    f.truncate()
+
+entry_exists = any('RGBPi-Extra.sh' in line for line in open(games_dat, 'r'))
+if not entry_exists:
+    with open(games_dat, 'a') as f:
+        f.write(data_to_insert)
+
+main_script = os.path.join(destination_dir, "application", "main.py")
+
+subprocess.Popen([python_exec, main_script])
+
+os.remove(__file__)
