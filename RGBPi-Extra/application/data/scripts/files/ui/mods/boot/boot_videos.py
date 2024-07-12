cc2f50ca20a8b5a03e02f265d3e8babf6daef8f0
forkymcforkface <36553759+forkymcforkface@users.noreply.github.com>
2024-07-12 13:35:55 -0700

    added boot video ability
    drop video files into /opt/rgbpi/ui/mods/boot/videos

---
+++ RGBPi-Extra/application/data/scripts/files/ui/mods/boot/boot_videos.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+def gen_video():
+    playlist_file = '/tmp/boot_video_playlist.m3u'
+    video_directory = os.path.join(os.path.dirname(__file__), 'videos')
+    video_files = []
+
+    for root, dirs, files in os.walk(video_directory):
+        for file in files:
+            video_files.append(os.path.join(root, file))
+
+    if video_files:
+        with open(playlist_file, 'w') as playlist:
+            for video_file in video_files:
+                playlist.write(f"{video_file}\n")
+
+    return playlist_file
+
+def play_videos():
+    playlist_file = gen_video()
+    if os.path.exists(playlist_file):
+        cvlc_command = [
+            'cvlc',
+            '--fullscreen',
+            '--play-and-exit',
+            '--no-osd',
+            '--quiet',
+            '--volume=256',
+            '--aout=alsa',
+            playlist_file
+        ]
+        subprocess.run(cvlc_command)
+
+if __name__ == "__main__":
+    play_videos()
