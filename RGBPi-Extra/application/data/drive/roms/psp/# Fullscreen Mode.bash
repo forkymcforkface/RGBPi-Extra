e8df84956188a0929ac68b42bd230b82b1b8c70f
Kev <36553759+forkymcforkface@users.noreply.github.com>
2024-08-20 14:04:14 -0700

    added pi5 configs

--- RGBPi-Extra/application/data/drive/roms/psp/# Fullscreen Mode.bash
+++ RGBPi-Extra/application/data/drive/roms/psp/# Fullscreen Mode.bash
@@ -2,27 +2,19 @@
 
 # New content for the first configuration file
 new_content_cfg1=$(cat <<EOF
-dynares_mode = "custom"
-video_fullscreen_x = "640"
-video_fullscreen_y = "480"
-custom_viewport_height = "480"
-custom_viewport_width = "640"
-aspect_ratio_index = "21"
+aspect_ratio_index = "24"
 run_ahead_enabled = "false"
 video_frame_delay_auto = "false"
 run_ahead_frames = "0"
 run_ahead_hide_warnings = "false"
 run_ahead_secondary_instance = "false"
 audio_dsp_plugin = "/opt/rgbpi/ui/data/dsp_filters/stereo.dsp"
-dynares_flicker_reduction = "true"
-video_font_path = "/opt/rgbpi/ui/raassets/fonts/native.ttf"
-video_font_size = "12.000000"
 EOF
 )
 
 
 # Path to the first configuration file
-cfg_file_path1="/media/usb1/gameconfig/sys_override/psp.cfg"
+cfg_file_path1="/opt/rgbpi/ui/tweaks/sys_overrides/psp.cfg"
 
 # Write new content to the first configuration file
 echo "$new_content_cfg1" > "$cfg_file_path1"
--- RGBPi-Extra/application/data/drive/roms/psp/# Widescreen Mode.bash
+++ RGBPi-Extra/application/data/drive/roms/psp/# Widescreen Mode.bash
@@ -2,19 +2,27 @@
 
 # New content for the first configuration file
 new_content_cfg1=$(cat <<EOF
-aspect_ratio_index = "24"
+dynares_mode = "custom"
+video_fullscreen_x = "320"
+video_fullscreen_y = "240"
+custom_viewport_height = "240"
+custom_viewport_width = "320"
+aspect_ratio_index = "21"
 run_ahead_enabled = "false"
 video_frame_delay_auto = "false"
 run_ahead_frames = "0"
 run_ahead_hide_warnings = "false"
 run_ahead_secondary_instance = "false"
 audio_dsp_plugin = "/opt/rgbpi/ui/data/dsp_filters/stereo.dsp"
+dynares_flicker_reduction = "true"
+video_font_path = "/opt/rgbpi/ui/raassets/fonts/native.ttf"
+video_font_size = "6.000000"
 EOF
 )
 
 
 # Path to the first configuration file
-cfg_file_path1="/media/usb1/gameconfig/sys_override/nds.cfg"
+cfg_file_path1="/opt/rgbpi/ui/tweaks/sys_overrides/psp.cfg"
 
 # Write new content to the first configuration file
 echo "$new_content_cfg1" > "$cfg_file_path1"
